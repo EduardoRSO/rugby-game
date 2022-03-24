c2ad8b3e91bed44c668e194a1a26a33df46e4c55
Eduardo Ribeiro Silva de Oliveira <edusillva784@gmail.com>
2022-03-23 23:01:18 -0300

    Redo the fork and add latest changes to attacker and defender strategy.
    Signed-off-by: Eduardo Ribeiro Silva de Oliveira <edusillva784@gmail.com>

--- src/attacker.c
+++ src/attacker.c
@@ -9,21 +9,156 @@
 // Main header
 #include "attacker.h"
 
+// New headers
+#include <stdlib.h>
+#include <time.h>
+
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+
+// Private Variables
+
+static int states[30] = {8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+       	          8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8};
+static int pointer = 0;
+static position_t attacker_last_position;
+static bool randomize = true;
+
+static int priority_DIR_UP = 8;
+static int priority_DIR_UP_RIGHT = 7;
+static int priority_DIR_RIGHT = 6;
+static int priority_DIR_DOWN_RIGHT = 5;
+static int priority_DIR_DOWN = 4;
+static int priority_DIR_DOWN_LEFT = 3;
+static int priority_DIR_LEFT = 2;
+static int priority_DIR_UP_LEFT = 1;
+
+// Private functions
+
+bool attacker_not_moved_to_position(position_t last, position_t actual){
+	return equal_positions(actual, last);
+}
+
+direction_t attacker_draw_back(){
+	if(pointer >= 0){
+		if(states[pointer] ==      priority_DIR_UP){ 		return (direction_t) DIR_DOWN;}
+		else if(states[pointer] == priority_DIR_UP_RIGHT){	return (direction_t) DIR_DOWN_LEFT;}
+		else if(states[pointer] == priority_DIR_RIGHT){		return (direction_t) DIR_LEFT;}
+		else if(states[pointer] == priority_DIR_DOWN_RIGHT){	return (direction_t) DIR_UP_LEFT;}
+		else if(states[pointer] == priority_DIR_DOWN){		return (direction_t) DIR_UP;}
+		else if(states[pointer] == priority_DIR_DOWN_LEFT){	return (direction_t) DIR_UP_RIGHT;}
+		else if(states[pointer] == priority_DIR_LEFT){		return (direction_t) DIR_RIGHT;}
+		else if(states[pointer] == priority_DIR_UP_LEFT){  	return (direction_t) DIR_DOWN_RIGHT;}
+		else /*this should not happen*/				return (direction_t) DIR_LEFT;
+	}
+	else{
+		/*this should not happen*/
+		return (direction_t) DIR_LEFT;
+	}
+}
+
+direction_t choose_attacker_direction(){
+	if(pointer >= 0){
+		if(states[pointer] == priority_DIR_UP) 			return (direction_t) DIR_UP;
+		else if(states[pointer] == priority_DIR_UP_RIGHT) 	return (direction_t) DIR_UP_RIGHT; 
+		else if(states[pointer] == priority_DIR_RIGHT) 		return (direction_t) DIR_RIGHT; 
+		else if(states[pointer] == priority_DIR_DOWN_RIGHT) 	return (direction_t) DIR_DOWN_RIGHT; 
+		else if(states[pointer] == priority_DIR_DOWN) 		return (direction_t) DIR_DOWN; 
+		else if(states[pointer] == priority_DIR_DOWN_LEFT) 	return (direction_t) DIR_DOWN_LEFT; 
+		else if(states[pointer] == priority_DIR_LEFT) 		return (direction_t) DIR_LEFT; 
+		else if(states[pointer] == priority_DIR_UP_LEFT) 	return (direction_t) DIR_UP_LEFT;
+	       	else{
+			pointer--;
+			return attacker_draw_back();
+		}	
+	}
+		/*this should not happen*/				return (direction_t) DIR_LEFT;
+}
+
+void  change_directions_priorities(position_t attacker, position_t defender){
+        if(attacker.i > defender.i){
+		if(attacker.j > defender.j){
+			//RUSH
+			priority_DIR_UP = 2;
+			priority_DIR_UP_RIGHT = 1;
+			priority_DIR_RIGHT = 8;
+			priority_DIR_DOWN_RIGHT = 7;
+			priority_DIR_DOWN = 6;
+			priority_DIR_DOWN_LEFT = 5;
+			priority_DIR_LEFT = 4;
+			priority_DIR_UP_LEFT = 3;
+		}else{
+			//DIAGONAL ESCAPE and RUSH
+			priority_DIR_UP = 1;
+			priority_DIR_UP_RIGHT = 5;
+			priority_DIR_RIGHT = 7;
+			priority_DIR_DOWN_RIGHT = 6 ;
+			priority_DIR_DOWN = 4;
+			priority_DIR_DOWN_LEFT = 8;
+			priority_DIR_LEFT = 3;
+			priority_DIR_UP_LEFT = 2;
+		
+		}
+	} else{
+		if(attacker.j > defender.j){
+			//RUSH
+			priority_DIR_UP = 3;
+			priority_DIR_UP_RIGHT = 5;
+			priority_DIR_RIGHT = 8;
+			priority_DIR_DOWN_RIGHT = 7;
+			priority_DIR_DOWN = 4;
+			priority_DIR_DOWN_LEFT = 6;
+			priority_DIR_LEFT = 1;
+			priority_DIR_UP_LEFT = 2;	
+		}else{
+			//DIAGONAL ESCAPE and RUSH
+			priority_DIR_UP = 2;
+			priority_DIR_UP_RIGHT = 5 ;
+			priority_DIR_RIGHT = 7;
+			priority_DIR_DOWN_RIGHT = 6 ;
+			priority_DIR_DOWN = 4 ;
+			priority_DIR_DOWN_LEFT = 1;
+			priority_DIR_LEFT = 3;
+			priority_DIR_UP_LEFT = 8;
+		
+		}
+	}
+        
+}
+
+
+direction_t randomize_starting_position(position_t last, position_t actual, Spy defender_spy){
+	if(attacker_not_moved_to_position(last, actual)){
+		attacker_last_position = actual;
+		randomize = false;
+		change_directions_priorities(actual, get_spy_position(defender_spy));
+		return choose_attacker_direction();
+	}else{
+		srand(time(NULL));
+		int random_number = rand() % 2;
+		attacker_last_position = actual;
+		if(random_number) return (direction_t) DIR_UP_RIGHT;
+	       	else		  return (direction_t) DIR_DOWN_RIGHT;
+	}
+}
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
-
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+direction_t execute_attacker_strategy(position_t attacker_position, Spy defender_spy) {
+//	printf("ATTACKER: (%ld, %ld), (%ld, %ld), pointer: %d, spy %ld, randomize %d\n",attacker_last_position.i, attacker_last_position.j, attacker_position.i, attacker_position.j, pointer, get_spy_number_uses(defender_spy), randomize);
+	if(randomize){
+		return randomize_starting_position(attacker_last_position, attacker_position, defender_spy);
+	}
+	if(attacker_not_moved_to_position(attacker_last_position,attacker_position)){
+		states[pointer]--;
+		return choose_attacker_direction();
+	}else{
+		pointer++;
+		attacker_last_position = attacker_position;
+		return choose_attacker_direction();
+	}
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -9,21 +9,88 @@
 // Main header
 #include "defender.h"
 
+// New headers
+#include <stdlib.h>
+#include <time.h>
+
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+
+// Private Variables
+static bool going_top = true;
+static bool going_left = true;
+static bool tried_left = false;
+static bool backtracking = false;
+static position_t defender_last_position;
+
+// Private Functions
+
+bool defender_not_moved_to_position(position_t last, position_t actual){
+        return equal_positions(actual, last);
+}
+
+void change_defender_direction(){
+	if(going_top) going_top = false;
+	else going_top = true;
+}
+
+direction_t choose_defender_slide(){
+	if(going_left) return (direction_t) DIR_LEFT;
+	return (direction_t) DIR_RIGHT;
+}
+
+direction_t choose_defender_direction(){
+	if(going_top) return (direction_t) DIR_UP;
+	return (direction_t) DIR_DOWN;
+}
+
+direction_t attacker_closest_position(position_t attacker, position_t defender){
+	going_left = false;
+	if(attacker.i > defender.i){
+		if(!backtracking && going_top) going_top = false;
+	}
+	else{
+		if(!backtracking && !going_top) going_top = true;	
+	}
+	return choose_defender_direction();
+}
+
+void force_defender_to_left_direction(position_t defender_position){
+	direction_t temp = choose_defender_direction();
+	defender_last_position.i = defender_position.i + temp.i;
+	defender_last_position.j = defender_position.j + temp.j;
+}
+
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+direction_t execute_defender_strategy(position_t defender_position, Spy attacker_spy) {
+	//printf("DEFENDER: (%ld, %ld), (%ld, %ld), going_top: %d, tried_left: %d, going_left: %d\n",defender_last_position.i, defender_last_position.j, defender_position.i, defender_position.j, going_top, tried_left, going_left);
+	srand(time(NULL));
+	if(rand() % 3 == 1 && !get_spy_number_uses(attacker_spy)){
+		position_t attacker_position = get_spy_position(attacker_spy);
+		return attacker_closest_position(attacker_position, defender_position);
+	}
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+	if(defender_not_moved_to_position(defender_last_position, defender_position)){
+		defender_last_position = defender_position;
+		if(tried_left){
+			force_defender_to_left_direction(defender_position);
+			backtracking = true;
+			tried_left = false;
+			return choose_defender_direction();
+		}
+		if(!backtracking) change_defender_direction();
+		tried_left = true;
+		return choose_defender_slide();
+	}else{
+		defender_last_position = defender_position;
+		backtracking = false;
+		tried_left = false;
+		return choose_defender_direction();
+	}
 }
 
 /*----------------------------------------------------------------------------*/
